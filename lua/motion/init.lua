592daf6c27e51989e90e2382eba0ea8b1e71b41a
vanaigr <vanaigranov@gmail.com>
2024-05-24 19:06:56 -0500

    initial commit

---
+++ lua/motion/init.lua
@@ -0,0 +1,140 @@
+local vim = vim
+
+local u = require('motion.util')
+
+local M = {}
+
+--- Modifies endpoints from range [`p1`, `p2`) or [`p2`, `p1`)
+--- for charwise visual selection. Positions are (1, 0) indexed.
+---
+--- @param p1 table<integer, integer>
+--- @param p2 table<integer, integer>
+--- @param context table? Context from `create_context()`
+--- @return boolean: Whether the selection can be created.
+function M.range_to_visual(p1, p2, context)
+    if not context then context = u.create_context() end
+    local lines = context.lines
+    local lines_count = context.lines_count
+
+    u.clamp_pos(p1, context)
+    u.clamp_pos(p2, context)
+
+    local sel = vim.api.nvim_get_option_value('selection', {})
+    if sel == 'exclusive' then
+        return not u.is_same_char(p1, p2, context)
+    end
+
+    local pos_f, pos_l
+    if u.pos_lt(p1, p2) then pos_f, pos_l = p1, p2
+    else pos_f, pos_l = p2, p1 end
+
+    u.move_to_cur(pos_f, context)
+    u.move_to_prev(pos_l, context)
+
+    if sel == 'inclusive' then
+        return not u.pos_lt(pos_l, pos_f)
+    end
+
+    assert(sel == 'old')
+    -- Do the best we can, since EOLs aren't always selectable at endpoints.
+    if pos_f[2] >= math.max(#lines[pos_f[1]], 1) and not context.virtualedit then
+        assert(pos_f[1] < lines_count)
+        pos_f[1] = pos_f[1] + 1
+        pos_f[2] = 0
+    end
+
+    if pos_l[2] > 0 and pos_l[2] >= #lines[pos_l[1]] then
+        pos_l[2] = #lines[pos_l[1]] - 1
+    end
+
+    return not u.pos_lt(pos_l, pos_f)
+end
+
+--- Modifies endpoints from range [`p1`, `p2`] or [`p2`, `p1`]
+--- for charwise visual selection. Positions are (1, 0) indexed.
+---
+--- @param p1 table<integer, integer>
+--- @param p2 table<integer, integer>
+--- @param context table? Context from `create_context()`
+---
+--- @return boolean: Whether the selection can be created.
+function M.range_inclusive_to_visual(p1, p2, context)
+    if not context then context = u.create_context() end
+    local lines = context.lines
+    local lines_count = context.lines_count
+
+    u.clamp_pos(p1, context)
+    u.clamp_pos(p2, context)
+
+    local sel = vim.api.nvim_get_option_value('selection', {})
+    if sel == 'inclusive' then return true end
+
+    local pos_f, pos_l
+    if u.pos_lt(p1, p2) then pos_f, pos_l = p1, p2
+    else pos_f, pos_l = p2, p1 end
+
+    if sel == 'exclusive' then
+        u.move_to_next(pos_l, context)
+        return true
+    end
+
+    assert(sel == 'old')
+
+    if pos_f[2] > 0 and pos_f[2] >= #lines[pos_f[1]] and not context.virtualedit then
+        if pos_f[1] >= lines_count then return false end -- last EOL in file
+        pos_f[1] = pos_f[1] + 1
+        pos_f[2] = 0
+    end
+
+    -- Note: virtualedit does nothing for end, so might as well move
+    if pos_l[2] > 0 then
+        local end_line_len = #lines[pos_l[1]]
+        if pos_l[2] >= end_line_len then
+            pos_l[2] = end_line_len - 1
+        end
+    end
+
+    return not u.pos_lt(pos_l, pos_f)
+end
+
+--- Sets charwise range [`pos`, `initial_pos`) or [`initial_pos`, `pos`)
+--- as positions for a custom motion. Positions are (1, 0) indexed.
+---
+--- @param pos table<integer, integer> Note: modified.
+--- @param context table? Context from `create_context()`
+--- @param initial_pos table<integer, integer>? Cursor position at the start of the current mapping. Defaults to the current cursor position. Note: modified.
+--- @return boolean: False if couldn't set the positions. Don't forget to reset the cursor position if you changed it previously!
+function M.motion_endpoint(pos, initial_pos, context)
+    if not context then context = u.create_context() end
+    local lines = context.lines
+
+    if not initial_pos then initial_pos = vim.api.nvim_win_get_cursor(0) end
+
+    u.clamp_pos(pos, context)
+    u.clamp_pos(initial_pos, context)
+
+    -- handle edgecase described below :h :delete
+    -- also incidentally handles edgecases in :h exclusive
+    -- (since end position cannot be in first col of the same line)
+    if pos[1] == initial_pos[1] then
+        if pos[2] < #lines[pos[1]] then
+            if u.is_same_char(pos, initial_pos, context) then return false end
+            vim.api.nvim_win_set_cursor(0, pos)
+            return true
+        end
+
+        -- Note: in cases like all,none   none doesn't matter
+        if context.virtualedit then
+            if pos[2] == initial_pos[2] then return false end
+            vim.api.nvim_win_set_cursor(0, pos)
+            return true
+        end
+    end
+
+    if not M.range_to_visual(pos, initial_pos, context) then return false end
+    u.visual_set_pos(pos, initial_pos)
+    return true
+end
+
+
+return M
---
+++ lua/motion/util.lua
@@ -0,0 +1,196 @@
+local vim = vim
+
+local Lines = {
+    __index = function(tab, k)
+        local line = vim.api.nvim_buf_get_lines(0, k-1, k, true)[1]
+        rawset(tab, k, line)
+        return line
+    end,
+}
+local Context = {
+    __index = function(tab, k)
+        if k == 'virtualedit' then
+            local ve = vim.split(
+                vim.api.nvim_get_option_value('virtualedit', {}),
+                ',', { plain = true }
+            )
+
+            local c = false
+            for _, v in ipairs(ve) do
+                if v == 'all' or v == 'onemore' then
+                    c = true
+                    break
+                end
+            end
+
+            rawset(tab, k, c)
+            return c
+        end
+    end
+}
+
+local M = {}
+
+--- @return table
+function M.create_context()
+    return setmetatable({
+        lines = setmetatable({}, Lines),
+        lines_count = vim.api.nvim_buf_line_count(0),
+    }, Context)
+end
+
+function M.visual_set_pos(p1, p2)
+    -- Enter visual first! Positions might be valid only in visual
+    vim.cmd([[noautocmd normal! v]])
+    vim.api.nvim_win_set_cursor(0, p1)
+    vim.cmd([[noautocmd normal! o]])
+    vim.api.nvim_win_set_cursor(0, p2)
+end
+
+--- Modifies given position to be clamped to the buffer and line length.
+---
+--- @param pos table<integer, integer>
+--- @param context table Context from `create_context()`
+--- @return table<integer, integer>: `pos`
+function M.clamp_pos(pos, context)
+    local lines_count = context.lines_count
+
+    if pos[1] < 1 then
+        pos[1] = 1
+        pos[2] = 0
+    elseif pos[1] > lines_count then
+        pos[1] = lines_count
+        pos[2] = #context.lines[lines_count]
+    elseif pos[2] < 0 then
+        pos[2] = 0
+    else
+        pos[2] = math.min(pos[2], #context.lines[pos[1]])
+    end
+
+    return pos
+end
+
+
+--- Checks whether `a` < `b`.
+--- @param a table<integer, integer>
+--- @param b table<integer, integer>
+--- @return boolean
+function M.pos_lt(a, b)
+    return a[1] < b[1] or (a[1] == b[1] and a[2] < b[2])
+end
+
+--- Checks whether given 2 positions are on the same character.
+--- Positions are (1, 0) indexed, clamped.
+---
+--- @param p1 table<integer, integer>
+--- @param p2 table<integer, integer>
+--- @param context table Context from `create_context()`
+--- @return boolean
+function M.is_same_char(p1, p2, context)
+    if p1[1] ~= p2[1] then return false end
+    if p1[2] == p2[2] then return true end
+    local line = context.lines[p1[1]]
+    return vim.fn.charidx(line, p1[2]) == vim.fn.charidx(line, p2[2])
+end
+
+--- Modifies `pos` to point to the first byte of the character that contains it.
+--- Composing characters are considered a part of one character.
+---
+--- @param pos table<integer, integer> (1, 0) indexed clamped position
+--- @param context table Context from `create_context()`
+--- @return table<integer, integer>: `pos`
+function M.move_to_cur(pos, context)
+    local lines = context.lines
+    local line = lines[pos[1]]
+
+    local ci = vim.fn.charidx(line, pos[2])
+    if ci < 0 then
+        pos[2] = #line
+    else
+        local bi = vim.fn.byteidx(line, ci)
+        assert(bi >= 0)
+        pos[2] = bi
+    end
+
+    return pos
+end
+
+--- Modifies `pos` to point to the first byte of the next character.
+--- Composing characters are considered a part of one character.
+---
+--- @param pos table<integer, integer> (1, 0) indexed clamped position
+--- @param context table Context from `create_context()`
+--- @return table<integer, integer>: `pos`
+function M.move_to_next(pos, context)
+    local lines = context.lines
+    local lines_count = context.lines_count
+    local line = lines[pos[1]]
+
+    if pos[2] == #line then
+        if pos[1] == lines_count then
+            pos[2] = #line
+        else
+            pos[1] = pos[1] + 1
+            pos[2] = 0
+        end
+        return pos
+    end
+
+    local ci = vim.fn.charidx(line, pos[2])
+    assert(ci >= 0)
+
+    local bi = vim.fn.byteidx(line, ci + 1)
+    assert(bi >= 0)
+    pos[2] = bi
+    return pos
+end
+
+--- Modifies `pos` to point to the first byte of the previous character.
+--- Composing characters are considered a part of one character.
+---
+--- @param pos table<integer, integer> (1, 0) indexed clamped position
+--- @param context table Context from `create_context()`
+--- @return table<integer, integer>: `pos`
+function M.move_to_prev(pos, context)
+    local lines = context.lines
+    local line = lines[pos[1]]
+
+    -- So many cases just bc charidx(line, #line) is not #chars in line...
+    -- Even though corresponding is true for byteidx...
+    if #line == 0 then
+        if pos[1] == 1 then
+            pos[2] = 0
+        else
+            pos[1] = pos[1] - 1
+            pos[2] = #lines[pos[1]]
+        end
+        return pos
+    elseif pos[2] == #line then
+        local ci = vim.fn.charidx(line, #line - 1)
+        assert(ci >= 0)
+        local bi = vim.fn.byteidx(line, ci)
+        assert(bi >= 0)
+        pos[2] = bi
+        return pos
+    end
+
+    local ci = vim.fn.charidx(line, pos[2])
+    assert(ci >= 0)
+    if ci == 0 then
+        if pos[1] == 1 then
+            pos[2] = 0
+        else
+            pos[1] = pos[1] - 1
+            line = lines[pos[1]]
+            pos[2] = #line
+        end
+    else
+        local bi = vim.fn.byteidx(line, ci - 1)
+        assert(bi >= 0)
+        pos[2] = bi
+    end
+
+    return pos
+end
+
+return M
